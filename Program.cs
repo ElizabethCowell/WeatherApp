5bddc7e0017adf77f2e50aeea0a4a8f703ced124
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-05-05 15:00:31 -0400

    current weather application

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace WeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weatherMap = new HttpClient();
+
+            Console.WriteLine("What is your area code?");
+
+            var zipCode = Console.ReadLine();
+
+            Console.WriteLine("What is your APIKey?");
+
+            var apiKey = Console.ReadLine();
+
+            var myWeather = weatherMap.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?&units=imperial&zip={zipCode}&appid={apiKey}").Result;
+            
+            var city = JObject.Parse(myWeather).GetValue("name").ToString();
+
+            //var description = JArray.Parse(JObject.Parse(myWeather).GetValue("weather").ToString()).ToString();
+            var weatherOb = JToken.Parse(myWeather).SelectToken("weather[0].description");
+
+
+            var current = JObject.Parse(myWeather).GetValue("main").ToString();
+            var temp = JObject.Parse(current).GetValue("temp").ToString();
+
+            Console.WriteLine($"Today in {city} the weather outside is {weatherOb} \n" +
+            $"The current tempurature is {temp}");
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class Weather
+    {
+       //{
+       // "coord":
+       //     {"lon":-82.9105,"lat":40.1146}
+
+       // "weather":
+       //     [{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04n"}]
+
+       //  "base":"stations"
+
+       //  "main":
+       //   {"temp":57.74,"feels_like":57.63,"temp_min":57,"temp_max":59,"pressure":969,"humidity":94}
+        
+       //  "visibility":10000
+            
+       //  "wind":{ "speed":10.09,"deg":336,"gust":20.98}
+    
+       //  "clouds":{ "all":100}
+        
+         //"dt":1620180092,"sys":{ "type":3,"id":2008059,"country":"US","sunrise":1620124044,"sunset":1620174557},"timezone":-14400,"id":0,"name":"Westerville","cod":200}
+    }
+}
